208adeb44ef8eb5dc2b11eb7619098ffbfe0190e
Monde <xytuple@gmail.com>
2020-07-08 12:09:25 +0200

    Updates

---
+++ precache-manifest.b4b587718e370655a32f9bb23bcb7b70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b93f6cd31c457aea909328a6a3474e77",
+    "url": "/paygate-demo/index.html"
+  },
+  {
+    "revision": "0a68b0e3a4bab7471439",
+    "url": "/paygate-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "782c29a5600fdd654f1e",
+    "url": "/paygate-demo/static/js/2.e3339124.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/paygate-demo/static/js/2.e3339124.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a68b0e3a4bab7471439",
+    "url": "/paygate-demo/static/js/main.206c54a3.chunk.js"
+  },
+  {
+    "revision": "26edd81d3eac1e86ae91",
+    "url": "/paygate-demo/static/js/runtime-main.7bd6e1e3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/paygate-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
